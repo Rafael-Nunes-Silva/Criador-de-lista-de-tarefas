9fd31a949d9f713a80f5bca768cd656a0cc0e10f
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-03-10 13:37:22 -0300

    Adicionada função responsável por ordenar os arquivos
    Anteriormente arquivos com a seguinte disposição:
    
    Exercicio 1.py
    Exercicio 2.py
    Exercicio 3.py
    Exercicio 10.py
    Exercicio 11.py
    
    Seriam listados no arquivo txt na seguinte ordem:
    
    Exercicio 1.py
    Exercicio 10.py
    Exercicio 11.py
    Exercicio 2.py
    Exercicio 3.py
    
    Agora eles serão listados na ordem correta

--- Criador de lista.py
+++ Criador de lista.py
@@ -1,11 +1,16 @@
-import os
 import glob
+import re
+
+def SortFiles(files):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(files, key=alphanum_key)
 
 address = input("Insira o nome da pasta em que se encontram os exercicios: ")
 
 outputContent = ""
 
-for fileAddress in glob.glob(f"{address}/*.py"):
+for fileAddress in SortFiles(glob.glob(f"{address}/*.py")):
     fileName = fileAddress[len(address)+1: len(fileAddress)-3]
     fileContent = open(fileAddress, "r").read()
     outputContent += f"{fileName}:\n{fileContent}\n\n"
