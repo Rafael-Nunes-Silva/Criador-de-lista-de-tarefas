e37cb160341120d7cd7e4604d118929f239e2e8f
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-03-11 18:22:10 -0300

    Atualizada a forma como o endereço da pasta é pedido
    O programa agora pede o endereço relativo da pasta aonde estão os arquivos de exercícios
    O programa também pede pela extensão dos arquivos de exercícios como: py, js, cs etc

--- Criador de lista.py
+++ Criador de lista.py
@@ -6,11 +6,12 @@ def SortFiles(files):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(files, key=alphanum_key)
 
-address = input("Insira o nome da pasta em que se encontram os exercicios: ")
+address = input("Insira o caminho da pasta em que se encontram os exercicios('lista 1' ou 'minhas listas/lista 2'): ")
+extension = input("Insira a extensão dos arquivos de exercicios(py): ")
 
 outputContent = ""
 
-for fileAddress in SortFiles(glob.glob(f"{address}/*.py")):
+for fileAddress in SortFiles(glob.glob(f"{address}/*{extension}")):
     fileName = fileAddress[len(address)+1: len(fileAddress)-3]
     fileContent = open(fileAddress, "r").read()
     outputContent += f"{fileName}:\n{fileContent}\n\n"
